ea7e55ef4df7b0ed442eb29ce33b23193c2a1e06
agustin <agustincardenascarrenho@gmail.com>
2020-05-09 00:47:35 -0400

    make views for library

--- bibloteca/urls.py
+++ bibloteca/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('status/' , views.index , name='index')
+    path('status/<int:status_id>' , views.index , name='index'),
 ]
--- bibloteca/views.py
+++ bibloteca/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Statu
+from .models import Statu , Library
 
-def index(request):
+def index(request, status_id):
     status = Statu.objects.all()
-    return HttpResponse(status)
+    library = Library.objects.all().filter(user = request.user.id).filter(book__statu = status_id)
+    return render(request,'index.html' , {'user' : request.user , 'library' : library , 'status' : status_id} )
+    #return HttpResponse(library)
+
