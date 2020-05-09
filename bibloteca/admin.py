77278f85d7c4563e4942a179b5b5b0b65ec35079
agustin <agustincardenascarrenho@gmail.com>
2020-05-08 22:00:33 -0400

    add models (book,library,progressBook,status)

--- bibloteca/admin.py
+++ bibloteca/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Statu
+from .models import Statu, Book , Library, ProgressBook
 
 admin.site.register(Statu)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(ProgressBook)
--- bibloteca/models.py
+++ bibloteca/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 class Statu(models.Model):
     descriptions = models.CharField(max_length = 100)
@@ -6,3 +8,18 @@ class Statu(models.Model):
     def __str__(self):
         return self.descriptions
     
+class Book(models.Model):
+    title = models.CharField(max_length = 300)
+    author = models.CharField(max_length = 300)
+    numPages = models.IntegerField()
+    statu = models.ForeignKey(Statu , on_delete=models.CASCADE)
+
+class Library(models.Model):
+    book = models.ForeignKey(Book , on_delete=models.CASCADE)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+class ProgressBook(models.Model):
+    currentPage = models.IntegerField()
+    date =  models.DateTimeField(default=datetime.now())
+    book = models.ForeignKey(Book , on_delete=models.CASCADE)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
