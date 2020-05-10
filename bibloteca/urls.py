b486812e3e0cbf7bf06eb072cbf6441567312892
agustin <agustincardenascarrenho@gmail.com>
2020-05-10 00:58:37 -0400

    add 'leidos , leyendo , porleer' pages and templates

--- bibloteca/urls.py
+++ bibloteca/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('status/<int:status_id>' , views.index , name='index'),
+    path('bibloteca/<int:status_id>' , views.bookList , name='bibloteca')
 ]
--- bibloteca/views.py
+++ bibloteca/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Statu , Library
+from .models import Statu , Library , ProgressBook
 
-def index(request, status_id):
-    status = Statu.objects.all()
+def index(request):
+    return render(request,'index.html' , {'user' : request.user } )
+
+def bookList(request, status_id):
     library = Library.objects.all().filter(user = request.user.id).filter(book__statu = status_id)
-    return render(request,'index.html' , {'user' : request.user , 'library' : library , 'status' : status_id} )
-    #return HttpResponse(library)
+    views={
+        1:'leidos.html',
+        2:'leyendo.html',
+        3:'porleer.html'
+    }
+    return render(request,views.get(status_id) , { 'library' : library , 'status' : status_id} )
+
 
