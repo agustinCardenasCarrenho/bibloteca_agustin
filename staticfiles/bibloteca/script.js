9013894d92cd6882dbbb57a60688a7f22a43abc2
agustin <agustincardenascarrenho@gmail.com>
2020-05-21 21:33:04 -0400

    WIP

--- staticfiles/bibloteca/script.js
+++ staticfiles/bibloteca/script.js
@@ -1,10 +1,12 @@
 var selects = document.getElementsByClassName('slc')
 var formUpdateProgress = document.getElementById('form')
+var formSearchBook = document.getElementById('formsearchbook')
 var iCantPages = document.getElementById('cantPages')
 var btns = document.getElementsByClassName('btn btn-primary')
 var currentPage = document.getElementById('currentPage')
 var book_id , cantpages
 var user_id = document.getElementsByName('user_id')[0].value
+var bookTitleSeacrh = document.getElementById('booktitleseacrh')
 
 
 //cambia el estado del libro
@@ -35,16 +37,19 @@ $("#modalUpdateProgress").on("hidden.bs.modal", function () {
     formUpdateProgress.reset()
 });
 
-formUpdateProgress.addEventListener('submit' , function(e){
-    e.preventDefault()
-    currentPage.value <= cantpages ? updateBookProgress(book_id, user_id, currentPage.value) : alert("La cantidad de paginas ingresadas en mayor a la cantidad de paginas del libro")
-})
 
+
+if(formUpdateProgress){
+    formUpdateProgress.addEventListener('submit' , function(e){
+        e.preventDefault()
+        currentPage.value <= cantpages ? updateBookProgress(book_id, user_id, currentPage.value) : alert("La cantidad de paginas ingresadas en mayor a la cantidad de paginas del libro")
+    })
+}
 
 
 
 function updateBookProgress(book_id ,user_id , currentPage){
-    let data = new FormData();
+    let data = new FormData()
     data.append('book_id', book_id)
     data.append('user_id', user_id)
     data.append('current_page', currentPage)
@@ -56,4 +61,42 @@ function updateBookProgress(book_id ,user_id , currentPage){
     }).then(function(e){
         location.reload() 
     })
+}
+
+
+if(formSearchBook){
+formSearchBook.addEventListener('submit', function(e){
+    let data = new FormData()
+    data.append('title' , bookTitleSeacrh.value)
+    data.append('csrfmiddlewaretoken' , document.getElementsByName('csrfmiddlewaretoken')[0].value)
+    e.preventDefault()
+    fetch('book/buscar',{
+        method : 'POST',
+        body : data,
+        credentials: 'same-origin',
+    }).
+    then(response => response.json()).
+    then( response => {
+        var divBooks = document.getElementById('divbooks')
+        divBooks.innerHTML = ''
+       if( response['book'].length == 0){
+            document.getElementById('msgerror').classList.remove('d-none')
+       }else{
+            response['book'].map( element => {
+                var image  = document.createElement('IMG')
+                var a = document.createElement('A')
+
+                document.getElementById('msgerror').setAttribute('class' , 'd-none')
+                image.setAttribute('src' , element.image)
+                image.setAttribute('class' , 'w-75 h-100')
+                a.setAttribute('href' , '/book/'+element.id)
+                a.setAttribute('class' , 'col-lg-4 col-xs-12 mt-3')
+                
+                a.append(image)
+                divBooks.appendChild(a)
+            })
+       }
+
+    })
+})
 }
